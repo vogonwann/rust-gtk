5432f6e305ae0785bd770cd9bb9f0dc7f987258d
Ivan Janjić <wannjanjic@gmail.com>
2023-08-06 01:28:11 +0200

    first commit

---
+++ my-gtk-app/src/main.rs
@@ -0,0 +1,42 @@
+use gtk::{prelude::*, Button};
+use gtk::{glib, Application, ApplicationWindow};
+
+const APP_ID: &str = "org.gtk_rs.MyGtkApp";
+
+fn main() -> glib::ExitCode {
+    // Create a new application
+    let app = Application::builder()
+        .application_id(APP_ID)
+        .build();
+    app.connect_activate(build_ui);
+
+    // Run the application
+    app.run()
+}
+
+fn build_ui(app: &Application) {
+    // Create a button with label and margins
+    let button = Button::builder()
+        .label("Press me!")
+        .margin_top(12)
+        .margin_bottom(12)
+        .margin_start(12)
+        .margin_end(12)
+        .build();
+
+    // Connect to "clicked" signal of `button`
+    button.connect_clicked(|button| {
+        // Set the label to "Hello World!" after the button has been clicked on
+        button.set_label("Hello World");
+    });
+
+    // Creeate a window and set the title
+    let window = ApplicationWindow::builder()
+        .application(app)
+        .title("My GTK App")
+        .child(&button)
+        .build();
+
+    // Present window
+    window.present();
+}
